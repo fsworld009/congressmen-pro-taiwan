4c42a2c44a933af7325aa7012b91fdbd9fd39567
Oliver Hsu <holivershU@icloud.com>
2015-09-26 21:43:43 -0400

    Initial checkin

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.listen(app.get('port'), function() {
 
 app.get('/db', function(request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    return;//console.log("connected: " + err);
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if(err) {
